e7b36f8f1abe5794c6928d89dd67537b42ef1b95
Cuong Nguyen <cuongnb14@gmail.com>
2026-04-11 10:59:45 +0700

    refactor: update linting configuration and remove unused files

--- {{cookiecutter.project_slug}}/common/exceptions/api_exceptions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import status
-from rest_framework.exceptions import APIException
-
-
-class UserAlreadyVerified(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = 'User already verified'
-    default_code = 'params_error'
